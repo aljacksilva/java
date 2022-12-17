aa7838da3bb6fc22afa6af2710379662bc462170
jackson <kinho160@homail.com>
2022-12-17 16:13:45 -0300

    boletin

---
+++ meu-primeiro-projeto-java/src/BoletinEstutantdil.java
@@ -0,0 +1,15 @@
+public class BoletinEstutantdil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+        if (mediaFinal < 6) {
+            System.out.println("REPROVADO");
+        }
+        else if (mediaFinal == 6) {
+            System.out.println("MÉDIA");
+        }
+        else {
+            System.out.println("APROVADO");
+        }
+    }
+
+}
