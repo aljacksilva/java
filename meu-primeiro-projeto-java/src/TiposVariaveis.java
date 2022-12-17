90ae6485319400c9f731944fb53da82858902897
jackson <kinho160@homail.com>
2022-12-17 17:06:39 -0300

    tipo variáveis

---
+++ meu-primeiro-projeto-java/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        short numerocurto = 1;
+        int numeroNormal = numerocurto;
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(salarioMinimo);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
