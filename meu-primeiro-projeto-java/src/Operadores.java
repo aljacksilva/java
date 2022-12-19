157111109ec3a4305e9f21a66485f43991ed52ef
jackson <kinho160@homail.com>
2022-12-19 14:23:17 -0300

    Editado

---
+++ meu-primeiro-projeto-java/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void  main(String[] args) {
+        String concatenacao = "?";
+
+        concatenacao = "Jackson" + "Almeida";
+
+        System.out.println(concatenacao);
+    }
+}
