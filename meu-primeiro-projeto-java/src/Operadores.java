302739336601522a136580f71c9a3848fc0032a7
jackson <kinho160@homail.com>
2022-12-19 14:54:07 -0300

    Operadores

--- meu-primeiro-projeto-java/src/Operadores.java
+++ meu-primeiro-projeto-java/src/Operadores.java
@@ -5,5 +5,25 @@ public class Operadores {
         concatenacao = "Jackson" + "Almeida";
 
         System.out.println(concatenacao);
+
+        int a, b;
+
+        a = 5;
+        b = 4;
+
+        String resultado = a==b ?"Verdadeiro" : "Falso";
+
+        System.out.println(resultado);
+
+        Boolean r1 = true;
+        Boolean r2 = false;
+
+        if (r1 && r2) {
+            System.out.println("Ambos são verdadeiros");
+        }
+
+        if (r1 || r2) {
+            System.out.println("Será falso");
+        }
     }
 }
