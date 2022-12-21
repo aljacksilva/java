45c141568898ac6df531c20788d56d25c14e3b8b
jackson <kinho160@homail.com>
2022-12-21 16:14:20 -0300

    Editado

--- meu-primeiro-projeto-java/src/Calculadora.java
+++ meu-primeiro-projeto-java/src/Calculadora.java
@@ -1,25 +1,25 @@
 
 public class Calculadora {
-    public static void soma(Double n1, Double n2) {
-        double resultado = n1 + n2;
+    public static void soma(int i, int j) {
+        double resultado = i + j;
 
         System.out.println("A soma é: "+resultado);
     }
 
-    public static void subtracao(Double n1, Double n2) {
-        double resultado = n1 - n2;
+    public static void subtracao(int i, int j) {
+        double resultado = i - j;
 
         System.out.println("A subtração é: "+resultado);
     }
 
-    public static void multiplicacao(Double n1, Double n2) {
-        double resultado = n1 * n2;
+    public static void multiplicacao(int i, int j) {
+        double resultado = i * j;
 
         System.out.println("A multiplicação é: "+resultado);
     }
 
-    public static void divisao(Double n1, Double n2) {
-        double resultado = n1 / n2;
+    public static void divisao(int i, int j) {
+        double resultado = i / j;
 
         System.out.println("A divisão é: "+resultado);
     }
