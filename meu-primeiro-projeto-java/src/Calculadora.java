c3d338d783aff0f1f7265dfe893f66276f32c6d1
jackson <kinho160@homail.com>
2022-12-21 16:09:08 -0300

    Calculadora

---
+++ meu-primeiro-projeto-java/src/Calculadora.java
@@ -0,0 +1,26 @@
+
+public class Calculadora {
+    public static void soma(Double n1, Double n2) {
+        double resultado = n1 + n2;
+
+        System.out.println("A soma é: "+resultado);
+    }
+
+    public static void subtracao(Double n1, Double n2) {
+        double resultado = n1 - n2;
+
+        System.out.println("A subtração é: "+resultado);
+    }
+
+    public static void multiplicacao(Double n1, Double n2) {
+        double resultado = n1 * n2;
+
+        System.out.println("A multiplicação é: "+resultado);
+    }
+
+    public static void divisao(Double n1, Double n2) {
+        double resultado = n1 / n2;
+
+        System.out.println("A divisão é: "+resultado);
+    }
+}
